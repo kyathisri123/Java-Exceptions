1b0f1620c9bf5732ff460082215d8691e1e64ef1
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 17:03:14 +0530

    Add files via upload

---
+++ ArithmeticExceptionExample.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+class ArithmeticExceptionExample {
+    public static void main(String[] args) {
+        int result = 10 / 0; // Generates ArithmeticException
+        System.out.println(result);
+    }
+}
