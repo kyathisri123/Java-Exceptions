67c9b12d6775bfd3569bbb506585b5e86d001098
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 17:03:51 +0530

    Add files via upload

---
+++ ClassNotFound.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+class ClassNotFound {
+    public static void main(String[] args) {
+        try {
+            Class.forName("UnknownClass");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException caught");
+        }
+    }
+}
+
