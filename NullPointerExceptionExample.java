e61a3727ad6cdacaa13753eae4fbe6660a13190e
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 17:12:04 +0530

    Add files via upload

---
+++ NullPointerExceptionExample.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+class NullPointerExceptionExample {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length()); 
+    }
+}
+
