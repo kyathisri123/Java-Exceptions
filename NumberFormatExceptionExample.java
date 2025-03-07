02d9e8876d2929f27e58e50d9ea536ec52a90cf8
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 17:12:22 +0530

    Add files via upload

---
+++ NumberFormatExceptionExample.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+class NumberFormatExceptionExample {
+    public static void main(String[] args) {
+        String str = "abc";
+        int num = Integer.parseInt(str); 
+    }
+}
+
