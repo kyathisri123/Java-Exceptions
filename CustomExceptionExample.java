c8fad208985b51dcc47237b79c4c535eb0d25a55
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 17:04:24 +0530

    Add files via upload

---
+++ CustomExceptionExample.java
@@ -0,0 +1,14 @@
+package Exceptions;
+
+class MyCustomException extends Exception {
+    MyCustomException(String message) {
+        super(message);
+    }
+}
+
+class CustomExceptionExample {
+    public static void main(String[] args) throws MyCustomException {
+        throw new MyCustomException("This is a custom exception");
+    }
+}
+
