332a760ab0fcb105774db4f79ea514dc0e6cf635
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 17:04:40 +0530

    Add files via upload

---
+++ FileNotFoundExceptionExample.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+import java.io.*;
+
+class FileNotFoundExceptionExample {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader file = new FileReader("nonexistent.txt");
+    }
+}
+
